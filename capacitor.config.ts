81a04207b9d9dcd2787bde94fe06b24bfd737598
ivoolizando <ivoomiranda@gmail.com>
2022-10-14 16:38:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Book Rosa Sugar Daddy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
